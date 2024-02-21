516e8477856cc337d21d0cf29bcb7988392a8b32
altugbozyaka2005 <altugbozyaka@gmail.com>
2024-02-21 16:15:11 +0200

    starting website

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import logo from './assets/RYM-logo.png'
+
+function App() {
+  return (
+    <>
+      <nav className='navbar'>
+        <div className='logo-container'>
+          <img src={logo} alt="RYM-logo" width="100" />
+        <h1>RYM</h1>
+        </div>
+        <h2>Redacciones</h2>
+        <h2>Mision</h2>
+        <h2>Contacto</h2>
+      </nav>
+
+    </>
+  );
+}
+
+export default App;
